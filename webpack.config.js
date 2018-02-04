74592d8ee83457b70f11fb4219b748ce2b11c518
Cory Reed <swashcap@gmail.com>
2018-02-04 14:40:24 -0800

    chore(pkg): upgrade Webpack, loader dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.scss$/,
-        loader: "style!css!sass"
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   }
